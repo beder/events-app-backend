5d0f4282b9d299e8d0a4a14808577882bb70de50
Beder Acosta Borges <bedermail@yahoo.com>
2024-04-01 19:37:41 +0200

    Refactor middleware to use getEvent instead of getUserEvent

--- packages/core/src/middleware.ts
+++ packages/core/src/middleware.ts
@@ -1,8 +1,8 @@
 import middy from "@middy/core";
 import { AsyncLocalStorage } from "async_hooks";
 import { APIGatewayProxyEventV2WithJWTAuthorizer } from "aws-lambda";
-import { getUserEvent } from "./getUserEvent";
 import { json } from "./json";
+import { getEvent } from "./getEvent";
 
 export const userStorage = new AsyncLocalStorage();
 
@@ -25,36 +25,33 @@ export const withUserStoredInContext = () => {
 
 export const withOwnershipCheck = () => {
   return {
-    before: (
+    before: async (
       handler: middy.Request<APIGatewayProxyEventV2WithJWTAuthorizer>
     ) => {
-      const id = parseInt(handler.event.pathParameters?.id ?? "");
-
-      if (id === undefined) {
-        return json({
-          statusCode: 400,
-          body: { message: "Missing id" },
-        });
-      }
-
       const userId = useUser();
 
       if (userId === undefined) {
-        return json({
-          statusCode: 401,
-          body: { message: "Unauthorized" },
-        });
+        return forbidden();
       }
 
-      // Check if the user owns the event
-      const event = getUserEvent(id, userId);
+      const id = parseInt(handler.event.pathParameters?.id ?? "");
 
-      if (!Boolean(event)) {
-        return json({
-          statusCode: 403,
-          body: { message: "Forbidden" },
-        });
+      if (id === undefined) {
+        return forbidden();
+      }
+
+      const event = await getEvent(id);
+
+      if (event?.userId !== userId) {
+        return forbidden();
       }
     },
   };
 };
+
+const forbidden = () => {
+  return json({
+    statusCode: 403,
+    body: { message: "Forbidden" },
+  });
+}
