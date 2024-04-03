98cdfedc776af3067155839eaf7f138b7841d245
Beder Acosta Borges <bedermail@yahoo.com>
2024-04-04 01:45:45 +0200

    Refactor listEvents to support searching by name and description

--- packages/core/src/listEvents.ts
+++ packages/core/src/listEvents.ts
@@ -1,6 +1,15 @@
 import { db } from "../drizzle/db";
 import { events } from "../drizzle/schema";
+import { like, or } from "drizzle-orm";
 
-export const listEvents = async () => {
-  return db.select().from(events);
+export const listEvents = async (q?: string) => {
+  const query = db.select().from(events);
+
+  if (!q) {
+    return query;
+  }
+
+  return query.where(
+    or(like(events.name, `%${q}%`), like(events.description, `%${q}%`))
+  );
 };
--- packages/functions/src/events/list.ts
+++ packages/functions/src/events/list.ts
@@ -2,8 +2,10 @@ import { json } from "@events-app-backend/core/src/utils/json";
 import { listEvents } from "@events-app-backend/core/src/listEvents";
 import { ApiHandler } from "sst/node/api";
 
-export const handler = ApiHandler(async () => {
-  const events = await listEvents();
+export const handler = ApiHandler(async (apiEvent) => {
+  const q = apiEvent.queryStringParameters?.q;
+
+  const events = await listEvents(q);
 
   return json({
     body: events,
